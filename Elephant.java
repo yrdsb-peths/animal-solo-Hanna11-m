36d7b5b84e4b6ce9b5dc05fe21957b8f25093fd3
Hanna11-m <440059605@gapps.yrdsb.ca>
2025-11-14 14:11:59 -0500

    Add Elephant

--- Elephant.java
+++ Elephant.java
@@ -1,9 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
+/**
+ * The Elephant, our hero.
+ * 
+ * @author Hanna Mao
+ * @version November 2025
+ */
 public class Elephant extends Actor
 {
     public void act()
     {
-        
+        move(1);
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -1,5 +1,11 @@
 import greenfoot.*;
 
+/**
+ * The World our hero lives.
+ * 
+ * @author Hanna Mao
+ * @version November 2025
+ */
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
@@ -7,6 +13,4 @@ public class MyWorld extends World {
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
     }
-    
-    
 }
