0aa1f564f703dba8e0f1656d3d176e74bd7112e5
Hanna11-m <440059605@gapps.yrdsb.ca>
2025-11-14 14:13:45 -0500

    Add Apple

--- MyWorld.java
+++ MyWorld.java
@@ -12,5 +12,8 @@ public class MyWorld extends World {
         
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
     }
 }
