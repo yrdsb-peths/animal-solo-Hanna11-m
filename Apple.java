2cbd3b8f51652a188d21a4f297b22acded49b282
Hanna11-m <440059605@gapps.yrdsb.ca>
2025-11-17 14:09:25 -0500

    Add gameOver method

--- Apple.java
+++ Apple.java
@@ -6,14 +6,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author Hanna Mao 
  * @version November 2025
  */
-public class Apple extends Actor
-{
-    /**
-     * Act - do whatever the Apple wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
+    public class Apple extends Actor
     {
-        setLocation(getX(), getY() + 1);
+        /**
+         * Act - do whatever the Apple wants to do. This method is called whenever
+         * the 'Act' or 'Run' button gets pressed in the environment.
+         */
+        public void act()
+        {
+            setLocation(getX(), getY() + 1);
+        }
     }
-}
