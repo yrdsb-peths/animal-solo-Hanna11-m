189368690d0f2586136cf115895dac74cb690de1
Hanna11-m <440059605@gapps.yrdsb.ca>
2025-11-24 17:23:26 -0500

    Update TitleScreen.java
    Add objects from visual interface and saving the world

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,10 +17,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, 300, 100);
+        prepare();
     }
-    
+
     /**
      * The main wrld act loop.
      */
@@ -32,5 +33,21 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,306,190);
+
+        Label label = new Label("Use <-- and --> to Move", 40);
+        addObject(label,303,264);
+
+        Label label2 = new Label("Press <space> to Start", 40);
+        addObject(label2,292,315);
     }
 }
